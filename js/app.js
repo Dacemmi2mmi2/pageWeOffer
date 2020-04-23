2d7755fb3bb9fff2e3bd226d8d703232ba9af413
Dacemmi2mmi2 <dshevchenko507@gmail.com>
2020-04-23 17:29:08 +0300

    add function app.js for load params screen and change files css

--- js/app.js
+++ js/app.js
@@ -22,6 +22,7 @@ const htmlElements = {
     closeModalGameSours: document.querySelector('.gameSours button'),
 }
 
+
 const variables = {
     counterDivs: 0,
     counter: 0,
@@ -34,7 +35,26 @@ const variables = {
     bgcColorBlue: '#00FFFF',
     positionModal: -100,
     positionSvgContainer: 0,
+    linkSvgJSON: 'js/svg.json',
 }
+
+
+const fnwh = function heigthWidth(data){
+    const paramsScreen = [0, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1150, 1300, 1450, 1600, 1950, 2600];
+    for(let i = 0; i <= paramsScreen.length; i++){
+        if(window.innerWidth < paramsScreen[i]){
+            htmlElements.svgContainer.innerHTML = data[paramsScreen[i]];
+            break;
+        }
+    }
+};
+fetch(variables.linkSvgJSON).then((response) => {return response.json()}).then((obj) => {fnwh(obj)});
+
+
+const sizeWindow = window.addEventListener('resize', () => {
+    fetch(variables.linkSvgJSON).then((response) => {return response.json()}).then((obj) => {fnwh(obj)});
+});
+
 
 const createDiv = setInterval(() =>{
     let div = document.createElement('div');
@@ -46,6 +66,7 @@ const createDiv = setInterval(() =>{
     variables.counterDivs === 100 ? clearInterval(createDiv) : '';
 }, 0.01);
 
+
 const coordinatesAndColorStars = function stars(color, htmlClass){
     let askDiv = document.querySelectorAll(htmlClass);
     askDiv.forEach((item) => {
@@ -55,6 +76,7 @@ const coordinatesAndColorStars = function stars(color, htmlClass){
     });
 }
 
+
 const callFunctionStars = setInterval(() => {
     variables.counter ++;
     variables.counter === 3 ? variables.counter = 0 : '';
@@ -62,6 +84,7 @@ const callFunctionStars = setInterval(() => {
     variables.counter === 1 ? coordinatesAndColorStars(variables.bgcColorRed, variables.classRedStars) : '';
     variables.counter === 2 ? coordinatesAndColorStars(variables.bgcColorBlue, variables.classBlueStars) : '';
 }, variables.timeOutCoordinatesDivs);
+
 
 const openModal = function openModalWindow(number){
     let someModal = document.querySelector(htmlElements.modalWindows[number]),
@@ -72,6 +95,8 @@ const openModal = function openModalWindow(number){
             variables.positionSvgContainer === -100 ? variables.positionModal ++ : variables.positionSvgContainer --;
         }, 5);
 }
+
+
 const closeModal = function closeModalWindow(number){
     let someModal = document.querySelector(htmlElements.modalWindows[number]),
         timeUp = setInterval(()=>{
@@ -82,22 +107,23 @@ const closeModal = function closeModalWindow(number){
         }, 5);
 }
 
-htmlElements.textSiteCompany.addEventListener('click', ()=>{openModal(0)});
-htmlElements.textInternetShop.addEventListener('click', ()=>{openModal(1)});
-htmlElements.textBlog.addEventListener('click', ()=>{openModal(2)});
-htmlElements.textPortfolio.addEventListener('click', ()=>{openModal(3)});
-htmlElements.textSocialNetwork.addEventListener('click', ()=>{openModal(4)});
-htmlElements.textForum.addEventListener('click', ()=>{openModal(5)});
-htmlElements.textLanding.addEventListener('click', ()=>{openModal(6)});
-htmlElements.textGameSours.addEventListener('click', ()=>{openModal(7)});
-htmlElements.closeModalSiteCompany.addEventListener('click', ()=>{closeModal(0)});
-htmlElements.closeModalInternetShop.addEventListener('click', ()=>{closeModal(1)});
-htmlElements.closeModalBlog.addEventListener('click', ()=>{closeModal(2)});
-htmlElements.closeModalPortfolio.addEventListener('click', ()=>{closeModal(3)});
-htmlElements.closeModalSocialNetwork.addEventListener('click', ()=>{closeModal(4)});
-htmlElements.closeModalForum.addEventListener('click', ()=>{closeModal(5)});
-htmlElements.closeModalLanding.addEventListener('click', ()=>{closeModal(6)});
-htmlElements.closeModalGameSours.addEventListener('click', ()=>{closeModal(7)});
+
+// htmlElements.textSiteCompany.addEventListener('click', ()=>{openModal(0)});
+// htmlElements.textInternetShop.addEventListener('click', ()=>{openModal(1)});
+// htmlElements.textBlog.addEventListener('click', ()=>{openModal(2)});
+// htmlElements.textPortfolio.addEventListener('click', ()=>{openModal(3)});
+// htmlElements.textSocialNetwork.addEventListener('click', ()=>{openModal(4)});
+// htmlElements.textForum.addEventListener('click', ()=>{openModal(5)});
+// htmlElements.textLanding.addEventListener('click', ()=>{openModal(6)});
+// htmlElements.textGameSours.addEventListener('click', ()=>{openModal(7)});
+// htmlElements.closeModalSiteCompany.addEventListener('click', ()=>{closeModal(0)});
+// htmlElements.closeModalInternetShop.addEventListener('click', ()=>{closeModal(1)});
+// htmlElements.closeModalBlog.addEventListener('click', ()=>{closeModal(2)});
+// htmlElements.closeModalPortfolio.addEventListener('click', ()=>{closeModal(3)});
+// htmlElements.closeModalSocialNetwork.addEventListener('click', ()=>{closeModal(4)});
+// htmlElements.closeModalForum.addEventListener('click', ()=>{closeModal(5)});
+// htmlElements.closeModalLanding.addEventListener('click', ()=>{closeModal(6)});
+// htmlElements.closeModalGameSours.addEventListener('click', ()=>{closeModal(7)});
 
 // var w = window.innerWidth
 // || document.documentElement.clientWidth
